7bffcad3d581fff1bba83a76aa4d1ed9d20531fd
Sarawuth Pitaksaveatchai <sarawuth.p@b-connex.net>
2016-04-04 14:12:59 +0700

    initial commit

---
+++ CarPark.Facts/TicketFacts.cs
@@ -0,0 +1,113 @@
+﻿using CarPark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarPark.Facts
+{
+    public class TicketFacts // Top-most level class (Public / Internal)
+    {
+        public class General // inner class / nested class can has all 5 access modifiers
+        {
+            [Fact]
+            public void BasicUsage()
+            {
+                // arrange -> จัดเตรียมสิ่งที่ต้องการเทส
+                Ticket t;
+
+                t = new Ticket();
+                t.PlateNo = "8315";
+                t.DateIn = new DateTime(2016, 4, 4, 9, 0, 0); // 9am
+                t.DateOut = DateTime.Parse("13:30"); // 1:30pm
+
+                // act
+
+                // assert ตรวจสอบ
+                Assert.Equal("8315", t.PlateNo);
+                Assert.Equal(9, t.DateIn.Hour);
+                Assert.Equal(13, t.DateOut.Hour);
+            }
+        }
+
+        public class ParkingFeeProperty
+        {
+            [Fact]
+            public void First15Minutes_Free()
+            {
+                // arrange
+                var t = new Ticket();
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("9:15");
+
+                // act
+                decimal fee = t.ParkingFee;
+
+                // assert
+                Assert.Equal(0m, fee);
+            }
+
+            [Fact]
+            public void WithInFirst3Hours_50Baht()
+            {
+                var t = new Ticket();
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("9:15:01");
+
+                decimal fee = t.ParkingFee;
+
+                Assert.Equal(50m, fee);
+            }
+
+            [Fact]
+            public void WithInFirst3HoursII_50Baht()
+            {
+                var t = new Ticket();
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("12:11");
+
+                decimal fee = t.ParkingFee;
+
+                Assert.Equal(50m, fee);
+            }
+
+            [Fact]
+            public void For4Hours_80Baht()
+            {
+                var t = new Ticket();
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("13:00");
+
+                decimal fee = t.ParkingFee;
+
+                Assert.Equal(80m, fee);
+            }
+
+            [Fact]
+            public void For6Hours_140Baht()
+            {
+                var t = new Ticket();
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("15:00");
+
+                decimal fee = t.ParkingFee;
+
+                Assert.Equal(140m, fee);
+            }
+
+            [Fact]
+            public void For6HoursExceed15Minutes_GetExtraHour()
+            {
+                var t = new Ticket();
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("15:15:01");
+
+                decimal fee = t.ParkingFee;
+
+                Assert.Equal(170m, fee);
+            }
+        }
+    }
+}
---
+++ CarPark.Models/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Models
+{
+    public class Ticket
+    {
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public decimal ParkingFee {
+            get
+            {
+                var TimeDiff = DateOut.TimeOfDay - DateIn.TimeOfDay;
+
+                decimal TotalMinutes = Convert.ToDecimal(TimeDiff.TotalMinutes);
+                int NetHours = Convert.ToInt16(TotalMinutes / 60);
+
+                if (TotalMinutes >= 195)
+                {
+                    return 50 + ((NetHours - 3) * 30) + ((((TotalMinutes - NetHours * 60) > 15) ? 1 : 0) * 30);
+                }
+                
+                if (TotalMinutes > 15)
+                    return 50m;
+                else
+                    return 0m;
+            }
+        }
+        public string PlateNo { get; set; }
+    }
+}
